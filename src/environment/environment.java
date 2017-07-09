997682ab03407957ab012844b7f7640f209d27ed
jamie.t.wolfe@gmail.com <BifgDoN6hJ5U>
2017-07-09 18:11:10 +1000

    Project test commit
    project directory has been edited

---
+++ src/environment/environment.java
@@ -0,0 +1,10 @@
+package environment;
+
+public class environment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/vassal/vassal.java
@@ -0,0 +1,10 @@
+package vassal;
+
+public class vassal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
